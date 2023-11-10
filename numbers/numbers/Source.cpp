53623ebbee90a2f0a68a4fc6e80b668dc36f52e9
unknown <C00301189@setu.ie>
2023-11-10 02:50:51 +0000

    odd number

--- numbers/numbers/Source.cpp
+++ numbers/numbers/Source.cpp
@@ -1,7 +1,17 @@
-//odd Sofiia Pankiv
+//even Sofiia Pankiv
+
+#include<iostream>
 
 int main()
 {
+	// user inouted number
+	int input =7;
 
+	std::cout << "Think of a number? " << std::endl;
+	std::cin >> input;
+
+	if (input % 2 == 1) {
+		std::cout << "The number was odd" << std::endl;
+	}
 	return 1;
 }
