2cbd011c7928f1ddc23713e2c41e39516e806b84
unknown <C00301189@setu.ie>
2023-11-10 02:57:46 +0000

    even number

--- numbers/numbers/Source.cpp
+++ numbers/numbers/Source.cpp
@@ -1,7 +1,18 @@
 //odd Sofiia Pankiv
 
+#include<iostream>
+
 int main()
 {
+	// user input number
+	int number = 8;
+
+	std::cout << "What is your number? " << std::endl;
+	std::cin >> number;
+
+	if (number % 2 == 0) {
+		std::cout << "The number is even" << std::endl;
+	}
 
 	return 1;
 }
